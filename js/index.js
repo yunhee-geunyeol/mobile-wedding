7661e3236ff510ca463def674bea59cdf0c51663
gykim0427 <gykim0427@naver.com>
2025-09-14 11:07:04 +0900

    메인화면 수정 및 반응형 작업

--- js/index.js
+++ js/index.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-  console.log("DOM is ready!");
+  console.log("jQuery : DOM is ready!");
 });
+
+function copyAccount(account) {
+  navigator.clipboard.writeText(account).then(() => {
+    alert("계좌번호가 복사되었습니다: " + account);
+  });
+}
