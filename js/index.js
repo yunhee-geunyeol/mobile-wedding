931ce4d4ea7a61fbce9b3b16c71eb83e07359999
gykim0427 <gykim0427@naver.com>
2025-09-28 17:47:55 +0900

    gallery section added

--- js/index.js
+++ js/index.js
@@ -1,8 +1,42 @@
-$(document).ready(function() {
-  console.log("jQuery : DOM is ready!");
+let currentIndex = 0;
+const $images = $(".galleryImage img");
+const $modal = $("#galleryModal");
+const $modalImg = $(".modal-image");
+
+// 이미지 클릭 시 모달 열기
+$images.on("click", function() {
+  currentIndex = $images.index(this);
+  $modal.css("display", "flex");
+  $modalImg.attr("src", $(this).attr("src"));
 });
 
-function copyAccount(account) {
+// 닫기 버튼
+$(".close").on("click", function() {
+  $modal.hide();
+});
+
+// 이전 버튼
+$(".prev").on("click", function(e) {
+  e.stopPropagation();
+  currentIndex = (currentIndex - 1 + $images.length) % $images.length;
+  $modalImg.attr("src", $images.eq(currentIndex).attr("src"));
+});
+
+// 다음 버튼
+$(".next").on("click", function(e) {
+  e.stopPropagation();
+  currentIndex = (currentIndex + 1) % $images.length;
+  $modalImg.attr("src", $images.eq(currentIndex).attr("src"));
+});
+
+// 모달 바깥 클릭 시 닫기
+$modal.on("click", function(e) {
+  if ($(e.target).is($modal)) {
+    $modal.hide();
+  }
+});
+
+function fn_copyAccount(account) {
   navigator.clipboard.writeText(account).then(() => {
     alert("계좌번호가 복사되었습니다: " + account);
   });
